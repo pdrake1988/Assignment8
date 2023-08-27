070fdda3b18f46d419c66a752f58364795870189
Paul Drake <pdrake1@me.com>
2023-08-27 16:41:54 -0500

    done

---
+++ Assignment8/Majority.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    internal class Majority
+    {
+        public void Frequency(int[] ints)
+        {
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+            foreach (int num in ints)
+            {
+                if (!counts.ContainsKey(num))
+                {
+                    counts.Add(num, 1);
+                }
+                else
+                {
+                    counts[num]++;
+                    Console.WriteLine(counts[num]);
+                }
+            }
+        }
+    }
+}
---
+++ Assignment8/Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    internal class Sum
+    {
+        public int[] twoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.length; i++)
+            {
+                for (int j = i + 1; j < nums.length; j++)
+                {
+                    if (nums[j] == target - nums[i])
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            // In case there is no solution, we'll just return null
+            return null;
+        }
+    }
+}
